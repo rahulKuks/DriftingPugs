16ce9455535f6a7f92587c6ed8152409ee105a47
John Doe <johndoe@default.com>
2017-06-11 16:35:57 -0700

    fixed compiler errors and seperated movement direction from HMD direction.

--- Assets/PlayerTouchpadController.cs
+++ Assets/PlayerTouchpadController.cs
@@ -1,16 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
-public class PlayerTouchpadController : MonoBehaviour {
+public class PlayerTouchpadController : MonoBehaviour
+{
+    public GameObject player;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Start()
+    {
+        //rcCarScript = rcCar.GetComponent<RC_Car>();
+        GetComponent<VRTK_ControllerEvents>().TriggerAxisChanged += new ControllerInteractionEventHandler(DoTriggerAxisChanged);
+        GetComponent<VRTK_ControllerEvents>().TouchpadAxisChanged += new ControllerInteractionEventHandler(DoTouchpadAxisChanged);
+
+        GetComponent<VRTK_ControllerEvents>().TriggerReleased += new ControllerInteractionEventHandler(DoTriggerReleased);
+        GetComponent<VRTK_ControllerEvents>().TouchpadTouchEnd += new ControllerInteractionEventHandler(DoTouchpadTouchEnd);
+
+        GetComponent<VRTK_ControllerEvents>().AliasMenuOn += new ControllerInteractionEventHandler(DoCarReset);
+    }
+
+    private void DoTouchpadAxisChanged(object sender, ControllerInteractionEventArgs e)
+    {
+        //rcCarScript.SetTouchAxis(e.touchpadAxis);
+    }
+
+    private void DoTriggerAxisChanged(object sender, ControllerInteractionEventArgs e)
+    {
+        //rcCarScript.SetTriggerAxis(e.buttonPressure);
+    }
+
+    private void DoTouchpadTouchEnd(object sender, ControllerInteractionEventArgs e)
+    {
+        //rcCarScript.SetTouchAxis(Vector2.zero);
+    }
+
+    private void DoTriggerReleased(object sender, ControllerInteractionEventArgs e)
+    {
+        //rcCarScript.SetTriggerAxis(0f);
+    }
+
+    private void DoCarReset(object sender, ControllerInteractionEventArgs e)
+    {
+        //rcCarScript.ResetCar();
+    }
 }
