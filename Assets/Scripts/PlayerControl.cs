80376956eaa6efe12ad8a6ab69beedd9dc91b39b
John Doe <johndoe@default.com>
2017-07-10 16:46:12 -0700

    Fixed sounds -Will

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -148,7 +148,8 @@ public class PlayerControl : MonoBehaviour
                 if (doTwist)
                     StartCoroutine("Rotate");
                 break;
-            case (PlayerState.Space):
+			case (PlayerState.Space):
+				SoundController.Instance.EnterSpace();
                 StartCoroutine("EarthGaze");
                 sprite.transform.SetParent(spriteParent, true);
 
