ebc112aed329be7f5c16c428e3e5c3ad7c5764ca
Rahul Kukreja <Rahul_Kukreja@thecdm.ca>
2017-07-12 11:15:23 -0700

    Invitation triggered only in forest and not in the sea/space. Animator controller inForest flag added. Sprite controller cleaned up and commented.

--- Assets/Scripts/SpriteController.cs
+++ Assets/Scripts/SpriteController.cs
@@ -4,22 +4,92 @@ using UnityEngine;
 
 public class SpriteController : MonoBehaviour
 {
-	private Animator parentAnim;
+	[Tooltip("The duration that the sprite needs to wait before it invites the user.")]
+	[SerializeField] float thresholdInvitationTime;
+
+	[Tooltip("The animator controller of the child sprite object.")]
 	[SerializeField] Animator childAnim;
+	private Animator parentAnim;
+
+	private int baseLayerIndex;
+	private bool invitationTrigger;
+	private Vector3 previousPosition;
+	private float idleTime;
+
 
     private void Awake()
     {
+		idleTime = 0;
+		previousPosition = transform.position;
         parentAnim = GetComponent<Animator>();
+		baseLayerIndex = parentAnim.GetLayerIndex ("BaseLayer");
     }
 
+
+	void Update()
+	{
+		// If the idle animation is active, then check the time passed and trigger the invitation animation if needed.
+		if(childAnim.GetBool("isIdling"))
+		{
+			invitationTrigger = CheckIdleTime ();
+
+			if (invitationTrigger) 
+			{
+				childAnim.SetTrigger ("isInviting");
+				invitationTrigger = false;
+				idleTime = 0;
+			}
+		}
+
+
+		
+	}
+
+
+	/// <summary>
+	/// Keeps track of the time spent idling and not moving. 
+	/// </summary>
+	/// <returns><c>true</c>, if time spent idling was greater than invitation threshold time, <c>false</c> otherwise.</returns>
+	bool CheckIdleTime ()
+	{
+		bool triggerAnimation = false;
+
+		if (transform.position == previousPosition) 
+		{
+			idleTime += Time.deltaTime;
+			if (idleTime >= thresholdInvitationTime) 
+			{
+				Debug.Log ("Triggering invitation");
+				idleTime = 0;
+				triggerAnimation = true;
+			}
+		}
+		else 
+		{
+			idleTime = 0;
+		}
+
+		previousPosition = transform.position;
+		return triggerAnimation;
+	}
+
+	/// <summary>
+	/// Trigger the next animation step to move the sprite onto the next checkpoint.
+	/// </summary>
+	/// <param name="index">Index.</param>
 	public void MoveAnimation(int index)
-    {
+	{
 		if (parentAnim.enabled == true) 
 		{
 			parentAnim.SetInteger("Checkpoint", index);
 			childAnim.SetBool ("isIdling", true);
+
+			if (index == 9) 
+			{
+				childAnim.SetBool ("inForest", false);
+			}
 		}
-    }
+	}
 
 	public void DisableParentAnimator()
 	{
@@ -30,4 +100,5 @@ public class SpriteController : MonoBehaviour
 	{
 		parentAnim.enabled = true;
 	}
+
 }
