7c41f7cb67fb2b29952fd932d5ed1ee0b1ce80d4
John Doe <johndoe@default.com>
2017-07-31 17:05:09 -0700

    Added crossfade for sea to space sounds

--- Assets/Scripts/SoundController.cs
+++ Assets/Scripts/SoundController.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class SoundController : MonoBehaviour
 {
-    public AudioSource forest;
-    public AudioSource sea;
-    public AudioSource space;
-    public AudioSource splash;
-    public AudioSource earthGaze;
+	[SerializeField] private AudioSource forest;
+	[SerializeField] private AudioSource sea;
+	[SerializeField] private AudioSource space;
+	[SerializeField] private AudioSource splash;
+	[SerializeField] private AudioSource earthGaze;
+	[SerializeField] private float crossfadeDuration = 1.0f;
 
     private static SoundController _instance;
     public static SoundController Instance
@@ -32,12 +33,31 @@ public class SoundController : MonoBehaviour
 
     public void EnterSpace()
     {
-		sea.Stop();
-		space.Play();
+		StartCoroutine(Crossfade(sea, space));
     }
 
     public void PlayEarthGaze()
     {
         earthGaze.Play();
     }
+
+	private IEnumerator Crossfade(AudioSource source1, AudioSource source2)
+	{
+		// Play 2nd source with volume starting at 0 to fade in
+		source2.volume = 0;
+		source2.Play();
+
+		float progress = 0.0f;
+
+		// Fade out 1st source & fade in 2nd source
+		while (progress <= crossfadeDuration)
+		{
+			progress += Time.deltaTime;
+			source1.volume = Mathf.Min(1 - (progress / crossfadeDuration), 1.0f);
+			source2.volume = Mathf.Min(progress / crossfadeDuration, 1.0f);
+			yield return new WaitForEndOfFrame();
+		}
+
+		source1.Stop();
+	}
 }
